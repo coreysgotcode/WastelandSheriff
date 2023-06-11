e7c024e4576d6c5531e12915d5b1226c8f17ecb8
Alex Hathaway <alex@hathaway.xyz>
2023-06-10 18:29:20 -0700

    finished project w/ audio

--- main.gd
+++ main.gd
@@ -5,28 +5,45 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func game_over():
+	$Music.stop()
+	$DeathSound.play()
+	
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	
+	$HUD.show_game_over()
+	
 func new_game():
+	$Music.play()
+	
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	
+	get_tree().call_group("mobs",  "queue_free")
+	
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+	
+	$HUD.update_score(score)
 
 func _on_score_timer_timeout():
 	score += 1
+	
+	$HUD.update_score(score)
 
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
--- player.gd
+++ player.gd
@@ -43,7 +43,7 @@ func _process(delta):
 		
 
 # collision signal for when player is hit
-func _on_body_entered(body):
+func _on_body_entered(_body):
 	hide()
 	hit.emit()
 	$CollisionShape2D.set_deferred("disabled",true)
