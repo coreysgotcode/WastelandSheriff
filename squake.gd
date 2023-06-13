4e676805490d9044cb360173e203b78e38c9a86c
Alex Hathaway <alex@hathaway.xyz>
2023-06-12 21:52:10 -0700

    Updated squake hit mechanics, spawning

--- squake.gd
+++ squake.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+signal hit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +14,9 @@ func _process(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
