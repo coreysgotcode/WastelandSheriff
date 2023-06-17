81de6b1a3a0ccbefa403e311042026ef83d2dc06
coreysgotcode <coreysgotcode@gmail.com>
2023-06-16 19:47:14 -0700

    Working Bullet Spawn

--- main.gd
+++ main.gd
@@ -1,11 +1,18 @@
 extends Node
 
+@export var bullet_scene: PackedScene
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Sheriff.start($StartPosition.position)
-	$Bullet.start($StartPosition.position)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _input(event):
+	if event.is_action_pressed("click"):
+		var bullet = bullet_scene.instantiate()
+		bullet.position = $Sheriff.position
+		add_child(bullet)
+		bullet.fire_bullet(event.position)
