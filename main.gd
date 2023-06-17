14b807d50854fbc22fd4ef33e5e1930768c8bc9f
coreysgotcode <coreysgotcode@gmail.com>
2023-06-16 19:10:12 -0700

    Working Bullet Fire and Bound

--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Sheriff.start($StartPosition.position)
+	$Bullet.start($StartPosition.position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
